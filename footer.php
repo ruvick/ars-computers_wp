072251ffdcd1835861276e7361df79bda7795e3a
Ruvick <rudikov.web@ya.ru>
2023-09-02 20:42:12 +0300

    Перенес основные файлы

--- footer.php
+++ footer.php
@@ -1,4 +1,36 @@
+<footer id="footer" class="footer">
+	<div class="footer__container _container">
 
+		<div class="footer__row d-flex">
+
+			<a href="index.html" class="logo-icon footer__logo">
+				<!-- <? bloginfo("url"); ?> -->
+				<p class="logo-icon__text">АРС КОМПЬЮТЕРЫ <span>Работаем с 1992 года</span></p>
+			</a>
+
+			<ul class="menu-list footer__menu-list d-flex">
+				<li class="menu-list__item"><a href="about.html" class="menu-list__link">О нас</a></li>
+				<li class="menu-list__item"><a href="catalog.html" class="menu-list__link">Каталог</a></li>
+				<li class="menu-list__item"><a href="payment.html" class="menu-list__link">Оплата и доставка</a></li>
+				<li class="menu-list__item"><a href="service.html" class="menu-list__link">Сервис</a></li>
+				<li class="menu-list__item"><a href="contacts.html" class="menu-list__link">Контакты</a></li>
+			</ul>
+
+			<div class="contacts d-flex">
+				<a href="tel:89538120099" class="contacts__phone">8 953 812 00 99</a>
+				<!-- <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>"><? echo $tel = carbon_get_theme_option("as_phone_1"); ?></a> -->
+				<p class="contacts__time-phone">Бесплатная консультация!</p>
+
+				<div class="footer__contacts-social contacts-social">Социальные сети:
+					<a href="https://vk.com/arscomps" class="contacts-social-link contacts-social-link--vk"></a>
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+</footer>
 </div>
 
 <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -2,30 +2,329 @@
 
 <?php get_template_part('template-parts/header-section');?>
 
-	<main class="page"> 
+<main class="page">
 
-			<!-- Вывод записей конкретной таксономии по id -->
-				<?
-					$args = array(
-						'posts_per_page' => 4,
-						'post_type' => 'ultra',
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'ultracat',
-								'field' => 'id',
-								'terms' => array(7)
-							)
-						)
-					);
-					$query = new WP_Query($args);
+<section id="slider-main" class="slider-main">
+	<div class="slider-bg-wrap">
+		<div class="slider-bg _swiper">
+			<div class="slider-bg__slide slider__slide slider-main__slide">
+				<div class="nuar_blk"></div>
+				<div class="slider-bg__container _container">
+					<h1 class="slider-bg__title">
+						Компьютеры и <br> <span>компьютерная техника</span>
+						<!-- <span>
+							КОНВЕЙЕРНОГО <br>
+							ОБОРУДОВАНИЯ
+						</span> -->
+					</h1>
+				</div>
+			</div>
+			<div class="slider-bg__slide slider__slide slider-main__slide-3">
+				<div class="nuar_blk"></div>
+				<div class="slider-bg__container _container">
+					<h1 class="slider-bg__title">
+						Ремонт и <br> <span>обслуживание компьютеров</span>
+						<!-- <span>
+							КОНВЕЙЕРНОГО <br>
+							ОБОРУДОВАНИЯ
+						</span> -->
+					</h1>
+				</div>
+			</div>
+		</div>
+		<div class="slider-bg__swiper-button-block swiper-button-block">
+			<!-- <div class="_container"> -->
+			<div class="swiper-button swiper-button-next slider-main-next"></div>
+			<div class="swiper-button swiper-button-prev slider-main-prev"></div>
+			<!-- </div> -->
+		</div>
+	</div>
+</section>
 
-					foreach( $query->posts as $post ){
-						$query->the_post();
-						get_template_part('template-parts/product-elem');
-					}  
-					wp_reset_postdata();
-				?>
+<section class="about section">
+	<div class="_container">
 
-	</main>
+		<h2 class="about__title title">
+			О компании
+		</h2>
+
+		<div class="about__subtitle">
+			<p> Компания «АРС» основана и эффективно работает на рынке г. Орла и Орловской области с 1992 года.
+				Форма собственности частная, что позволяет работать гибко и быстро
+				с минимальными накладными расходами.
+				Сохранён основной костяк сотрудников с момента основания компании.
+				Огромная база поставщиков - дистрибьюторов. Оперативность поставки, от двух дней.
+				Возможность(спец.контракты) получения спеццен от производителей
+				оборудования.
+			</p>
+		</div>
+
+	</div>
+</section>
+
+<section class="products-sec section">
+	<div class="_container">
+		<h2 class="products-sec__title title">Продукция</h2>
+		<div class="products-sec__row">
+
+			<div class="products-sec__column">
+				<div class="products-sec__card">
+					<a href="catalog.html" class="products-sec__card-img _ibg">
+						<picture><source srcset="img/card/01.webp" type="image/webp"><img src="img/card/01.jpg?_v=1693674569456" alt=""></picture>
+					</a>
+					<div class="products-sec__card-descp">
+						<h4 class="products-sec__card-descp-title">
+							Компьютеры
+						</h4>
+						<ul class="products-sec__card-descp-list">
+							<li class="products-sec__card-descp-list-item">от 7800р</li>
+						</ul>
+					</div>
+					<div class="products-sec__card-btn">
+						<a href="catalog.html" class="products-sec__card-btn-link btn">Подробнее</a>
+						<a href="img/popup.jpg" download class="products-sec__card-btn-link btn btn--price">Прайс-лист</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="products-sec__column">
+				<div class="products-sec__card">
+					<a href="catalog.html" class="products-sec__card-img _ibg">
+						<picture><source srcset="img/card/02.webp" type="image/webp"><img src="img/card/02.jpg?_v=1693674569456" alt=""></picture>
+					</a>
+					<div class="products-sec__card-descp">
+						<h4 class="products-sec__card-descp-title">
+							Принтеры
+						</h4>
+						<ul class="products-sec__card-descp-list">
+							<li class="products-sec__card-descp-list-item">от 8800 р</li>
+						</ul>
+					</div>
+					<div class="products-sec__card-btn">
+						<a href="catalog.html" class="products-sec__card-btn-link btn">Подробнее</a>
+						<a href="img/popup.jpg" download class="products-sec__card-btn-link btn btn--price">Прайс-лист</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="products-sec__column">
+				<div class="products-sec__card">
+					<a href="catalog.html" class="products-sec__card-img _ibg">
+						<picture><source srcset="img/card/03.webp" type="image/webp"><img src="img/card/03.jpg?_v=1693674569456" alt=""></picture>
+					</a>
+					<div class="products-sec__card-descp">
+						<h4 class="products-sec__card-descp-title">
+							МФУ
+						</h4>
+						<ul class="products-sec__card-descp-list">
+							<li class="products-sec__card-descp-list-item">от 10 000р</li>
+						</ul>
+					</div>
+					<div class="products-sec__card-btn">
+						<a href="catalog.html" class="products-sec__card-btn-link btn">Подробнее</a>
+						<a href="img/popup.jpg" download class="products-sec__card-btn-link btn btn--price">Прайс-лист</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="products-sec__column">
+				<div class="products-sec__card">
+					<a href="catalog.html" class="products-sec__card-img _ibg">
+						<picture><source srcset="img/card/04.webp" type="image/webp"><img src="img/card/04.jpg?_v=1693674569456" alt=""></picture>
+					</a>
+					<div class="products-sec__card-descp">
+						<h4 class="products-sec__card-descp-title">
+							Ноутбуки
+						</h4>
+						<ul class="products-sec__card-descp-list">
+							<li class="products-sec__card-descp-list-item">от 30 000р</li>
+						</ul>
+					</div>
+					<div class="products-sec__card-btn">
+						<a href="catalog.html" class="products-sec__card-btn-link btn">Подробнее</a>
+						<a href="img/popup.jpg" download class="products-sec__card-btn-link btn btn--price">Прайс-лист</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="products-sec__column">
+				<div class="products-sec__card">
+					<a href="catalog.html" class="products-sec__card-img _ibg">
+						<picture><source srcset="img/card/05.webp" type="image/webp"><img src="img/card/05.jpg?_v=1693674569456" alt=""></picture>
+					</a>
+					<div class="products-sec__card-descp">
+						<h4 class="products-sec__card-descp-title">
+							Мониторы
+						</h4>
+						<ul class="products-sec__card-descp-list">
+							<li class="products-sec__card-descp-list-item">от 8000р</li>
+						</ul>
+					</div>
+					<div class="products-sec__card-btn">
+						<a href="catalog.html" class="products-sec__card-btn-link btn">Подробнее</a>
+						<a href="img/popup.jpg" download class="products-sec__card-btn-link btn btn--price">Прайс-лист</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="products-sec__column">
+				<div class="products-sec__card">
+					<a href="catalog.html" class="products-sec__card-img _ibg">
+						<picture><source srcset="img/card/06.webp" type="image/webp"><img src="img/card/06.jpg?_v=1693674569456" alt=""></picture>
+					</a>
+					<div class="products-sec__card-descp">
+						<h4 class="products-sec__card-descp-title">
+							Комплектующие
+						</h4>
+						<ul class="products-sec__card-descp-list">
+							<li class="products-sec__card-descp-list-item">от 5 000р</li>
+						</ul>
+					</div>
+					<div class="products-sec__card-btn">
+						<a href="catalog.html" class="products-sec__card-btn-link btn">Подробнее</a>
+						<a href="img/popup.jpg" download class="products-sec__card-btn-link btn btn--price">Прайс-лист</a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+<section class="service section">
+	<div class="nuar_blk"></div>
+	<div class="_container">
+		<h2 class="service__title title">Преимущества</h2>
+
+		<div class="service__row">
+
+			<div class="service__column">
+				<div class="service__card">
+					<span class="service__card-icon service__card-icon_01"></span>
+					<h4 class="service__card-title">
+						Официальные поставки и новые комплектующие
+					</h4>
+				</div>
+			</div>
+
+			<div class="service__column">
+				<div class="service__card">
+					<span class="service__card-icon service__card-icon_02"></span>
+					<h4 class="service__card-title">
+						Контроль качества комплектующих
+					</h4>
+				</div>
+			</div>
+
+			<div class="service__column">
+				<div class="service__card">
+					<span class="service__card-icon service__card-icon_03"></span>
+					<h4 class="service__card-title">
+						Техническая поддержка на протяжении всего срока эксплуатации ПК
+					</h4>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+</section>
+
+<section class="completed-projects section">
+	<div class="_container">
+		<h2 class="completed-projects__title title">Услуги</h2>
+		<div class="completed-projects__row">
+
+			<div class="completed-projects__column">
+				<a href="service.html" class="completed-projects__card">
+					<div class="completed-projects__card-img _ibg">
+						<picture><source srcset="img/completed/01.webp" type="image/webp"><img src="img/completed/01.jpg?_v=1693674569456" alt=""></picture>
+					</div>
+					<div class="completed-projects__card-descp completed-projects__card-descp_red">
+						<h5 class="completed-projects__card-descp-title">
+							Гарантийный ремонт
+						</h5>
+						<!-- <a href="#" class="completed-projects__card-descp-link">Подробнее...</a> -->
+					</div>
+				</a>
+			</div>
+
+			<div class="completed-projects__column">
+				<a href="service.html" class="completed-projects__card">
+					<div class="completed-projects__card-img _ibg">
+						<picture><source srcset="img/completed/02.webp" type="image/webp"><img src="img/completed/02.jpg?_v=1693674569456" alt=""></picture>
+					</div>
+					<div class="completed-projects__card-descp">
+						<h5 class="completed-projects__card-descp-title">
+							Послегарантийный ремонт
+						</h5>
+						<!-- <a href="#" class="completed-projects__card-descp-link">Подробнее...</a> -->
+					</div>
+				</a>
+			</div>
+
+			<div class="completed-projects__column">
+				<a href="service.html" class="completed-projects__card">
+					<div class="completed-projects__card-img _ibg">
+						<picture><source srcset="img/completed/03.webp" type="image/webp"><img src="img/completed/03.jpg?_v=1693674569456" alt=""></picture>
+					</div>
+					<div class="completed-projects__card-descp">
+						<h5 class="completed-projects__card-descp-title">
+							Заправка картриджей
+						</h5>
+						<!-- <a href="#" class="completed-projects__card-descp-link">Подробнее...</a> -->
+					</div>
+				</a>
+			</div>
+
+		</div>
+
+		<div class="completed-projects__help help-wrap">
+			<div class="help-wrap__name">
+				<h2 class="help-wrap__name-title">
+					Вас заинтересовали <br>
+					наши услуги?
+				</h2>
+				<p class="help-wrap__name-subtitle">Наши специалисты помогут:</p>
+			</div>
+			<div class="help-wrap__descp">
+				<ul class="help-wrap__descp-list">
+					<li class="help-wrap__descp-list-item">Подобрать необходимое оборудование</li>
+					<li class="help-wrap__descp-list-item">Провести технический аудит</li>
+					<li class="help-wrap__descp-list-item">Произвести ремонт техники</li>
+					<li class="help-wrap__descp-list-item">Заключить договор на абонентское обслуживание</li>
+				</ul>
+				<!-- <div class="help-wrap__btn"> -->
+				<a href="#callback" class="help-wrap__btn-link _popup-link btn">Заказать звонок</a>
+				<!-- </div> -->
+			</div>
+		</div>
+
+	</div>
+</section>
+
+<section id="consult" class="consult">
+<div class="_container">
+<h2 class="consult__title title">
+Остались вопросы по нашим товарам или нужна помощь <br>
+в подборе оборудования?
+</h2>
+<p class="consult__subtitle ">Оставьте заявку и наши менеджеры свяжутся с Вами в ближайшее время.</p>
+<form action="#" class="consult__form">
+<div class="consult__form-line form__line">
+	<input id="name" autocomplete="off" type="text" name="form[]" data-error="Заполните поле" data-value="Имя"
+		class="consult__form-input input">
+
+	<input id="tel2" autocomplete="off" type="text" name="form[]" data-error="Заполните поле" data-value="Телефон"
+		class="consult__form-input input _phone _req">
+
+	<button type="submit" class="consult__form-btn btn">Заявка на расчет стоимости</button>
+
+</div>
+</form>
+</div>
+</section>
+
+</main>
 
 <?php get_footer(); ?> 
