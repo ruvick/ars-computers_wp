55db8f743635b1979003abfa2b62c29b99060e8d
Ruvick <rudikov.web@ya.ru>
2023-09-10 12:24:19 +0300

    Структура внутренних страниц

--- page.php
+++ page.php
@@ -15,17 +15,17 @@
 
 get_header(); ?>
 
-<?php get_template_part('template-parts/header-section');?>
+<?php get_template_part('template-parts/header-section');?> 
 
 	<main id="primary" class="page site-main"> 
 
-		<section class="content"> 
+		<section class="page-text section content"> 
 			<div class="_container">
 
 			<?php
-			if ( function_exists('yoast_breadcrumb') ) {
-				yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-			}
+				if ( function_exists('yoast_breadcrumb') ) {
+					yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+				}
 			?> 
 
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
@@ -36,6 +36,9 @@ get_header(); ?>
 
 			</div>
 		</section>
+
+		<?php get_template_part('template-parts/consult');?> 
+
 	</main>
 
 <?php get_footer();
