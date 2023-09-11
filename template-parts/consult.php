386a91d55b196108b98f648a8b5dd83c70fcb27e
Ruvick <rudikov.web@ya.ru>
2023-09-12 00:06:37 +0300

    Настроил отправку второй формы

--- template-parts/consult.php
+++ template-parts/consult.php
@@ -4,15 +4,20 @@
       Остались вопросы по нашим товарам или нужна помощь <br>
       в подборе оборудования?
     </h2>
-    <p class="consult__subtitle ">Оставьте заявку и наши менеджеры свяжутся с Вами в ближайшее время.</p>
-    <form action="#" class="consult__form">
+    <p class="consult__subtitle ">Оставьте заявку и наши менеджеры свяжутся с Вами в ближайшее время.</p> 
+
+    <form id="consult_form" class="consult__form form">
+
       <div class="consult__form-line form__line">
-        <input id="name" autocomplete="off" type="text" name="form[]" data-error="Заполните поле" data-value="Имя"
-          class="consult__form-input input">
-        <input id="tel2" autocomplete="off" type="text" name="form[]" data-error="Заполните поле" data-value="Телефон"
-          class="consult__form-input input _phone _req">
-        <button type="submit" class="consult__form-btn btn">Заявка на расчет стоимости</button>
+				<input type="hidden" name = "form_name" data-valuem = "Название формы" value = "Заявка на консультацию">
+				<input type="hidden" name = "page_lnk" data-valuem = "Адрес страницы" value = "<? echo (is_home())?"http://arscomp.ru/":get_the_permalink()?>">
+				<input type="text" name="name" required data-valuem = "Имя*" placeholder="Имя*" class="consult__form-input input">
+				<input type="tel" name="tel" required data-valuem = "Телефон*" placeholder="Телефон*" class="consult__form-input input headen_form_blk">
+				<input disabled type="text" placeholder="Сообщение отправлено*" class="consult__form-input input SendetMsg form_msg" style="display:none;">
+        <button type="submit" class="consult__form-btn btn new_send_btn" data-formmsg="Заявка на консультацию" data-formid="consult_form">Заявка на расчет стоимости</button>
       </div>
+
     </form>
+
   </div>
 </section>
